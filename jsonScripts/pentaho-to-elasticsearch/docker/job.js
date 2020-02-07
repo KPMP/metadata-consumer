5297c6d48a77730a4f7ad98e63b943c9ab916b4d
Ross Smith <rossmith@Rosss-MacBook-Pro.local>
2020-02-07 17:17:08 -0500

    KPMP-1593 pentaho script is kicking

--- jsonScripts/pentaho-to-elasticsearch/docker/job.js
+++ jsonScripts/pentaho-to-elasticsearch/docker/job.js
@@ -3,102 +3,90 @@ const MongoClient = require('mongodb').MongoClient;
 const util = require('util');
 const fs = require('fs');
 const readFile = util.promisify(fs.readFile);
+require('dotenv').config();
 
-//Ensure we have all required arguments
-let requiredEnvNames = ['ES_HOST', 'ES_LOG', 'ES_VERSION', 'ES_INDEX', 'MONGO_URL', 'MONGO_DBNAME', 'METADATA_JSON_PATH'];
-let missingEnvNames = requiredEnvNames.filter((e) => !process.env.hasOwnProperty(e) || !process.env[e].length);
-
-if(missingEnvNames.length > 0) {
-  console.log('!!! ERROR: Some required env vars are missing, noted with ! below:');
-  for(let i = 0; i < requiredEnvNames.length; i++)  {
-    console.log((missingEnvNames.indexOf(requiredEnvNames[i]) > -1 ? '! ' : '- ') + requiredEnvNames[i]);
-  }
-  process.exit();
-}
-
-let propertyFixes = null;
 const PROPERTY_FIXES_JSON_PATH = './property-fixes.json';
 
-//Create an elasticsearch client
 const esClient = new require('elasticsearch').Client({
   host: process.env.ES_HOST,
   log: process.env.ES_LOG,
   apiVersion: process.env.ES_VERSION
 });
 
-async function getApiCallBody(db, packageId) {
-  return new Promise((res, rej) => {
-    var packageCollection = db.collection("packages");
-    var body = [];
+let propertyFixes = null;
+const requiredEnvNames = ['ES_HOST', 'ES_LOG', 'ES_VERSION', 'ES_INDEX', 'MONGO_URL', 'MONGO_DBNAME', 'METADATA_JSON_PATH'];
 
-    packageCollection.findOne({ _id: packageId }, async function(err, doc) {
-      assert.equal(null, err);
-
-      try {
-        doc.metadata = await getMetadataJson(process.env.METADATA_JSON_PATH, packageId);
-        res(await getFlattenedEntries(doc, db));
+function init(requiredEnvNames) {
+  return new Promise(async function(res, rej) {
+    let missingEnvNames = requiredEnvNames.filter((e) => !process.env.hasOwnProperty(e) || !process.env[e].length);
+    if(missingEnvNames.length > 0) {
+      console.log('!!! ERROR: Some required env vars are missing, noted with ! below:');
+      for(let i = 0; i < requiredEnvNames.length; i++)  {
+        console.log((missingEnvNames.indexOf(requiredEnvNames[i]) > -1 ? '! ' : '- ') + requiredEnvNames[i]);
       }
-
-      catch(err) {
-        console.error('!!! Error in getApiCallBody: ', err);
-        rej(err);
-      }
-    });
+      process.exit();
+    }
+    
+    propertyFixes = await readFile(PROPERTY_FIXES_JSON_PATH);
+    propertyFixes = JSON.parse(propertyFixes);
+    res();
   });
 }
 
-async function getMetadataJson(path, packageId) {
-  //Use the file system to read the newline-delimited JSON metadata records to an array
+async function getApiCallBody(db, packageId) {
   return new Promise(async function(res, rej) {
-    try {
-      let metadataRows = await readFile(path).split(/[\r\n]+/);
-      console.log('+++ getMetadataJson -> ', metadataRows);
-      let output = [];
-      for(metadata in metadataRows) {
-        if(packageId.equals(metadata.packageID)) {
-          output.push(metadata);
-        }
-      }
+    let packageCollection = db.collection("packages");
+    let body = [];
 
-      res(output);
+    try {
+      let metadata = await readMetadataFile();
+      let packagePredicates = metadata.map(function(meta) { return {_id: meta.packageID}; });
+
+      packageCollection.find({ $or: packagePredicates }).toArray(async function(err, docs) {
+        if(err) {
+          rej(err);
+          return;
+        }
+
+        for(let i = 0; i < docs.length; i++) {
+          let doc = docs[i];
+          doc.metadata = metadata.filter((meta) => meta.packageID === doc._id);
+          body = body.concat(await getFlattenedEntries(doc, db));
+        }
+
+        res(body);
+      });
     }
 
     catch(err) {
-      console.error('!!! Error in getMetadataJson: ', err);
+      console.error('!!! Error in getApiCallBody: ', err);
       rej(err);
     }
   });
 }
 
-async function fixPropertyName(prop) {
+async function readMetadataFile() {
   return new Promise(async function(res, rej) {
     try {
-      if(propertyFixes == null) {
-        propertyFixes = await readFile(PROPERTY_FIXES_JSON_PATH);
-      }
-
-      let fixedProp = prop.slice(0);
-
-      for(let replaceKey in propertyFixes.replace) {
-        fixedProp = fixedProp.replace(new RegExp(replaceKey, 'g'), propertyFixes.replace[replaceKey]);
-      }
-
-      let fixedPropWords = fixedProp.split(/(?=[A-Z])/);
-      for(let i = 0; i < fixedPropWords.length; i++) {
-        if(i == 0 || "_" === fixedPropWords[i - 1].slice(-1)) {
-          fixedPropWords[i] = fixedPropWords[i].slice(0).toLowerCase() + fixedPropWords[i].slice(1);
-        }
-      }
-
-      if(propertyFixes.packagePropertiesOnMetadata.indexOf(prop)) {
-        fixedPropWords.unshift('metadataFile_');
-      }
-
-      res(fixedPropWords.join(''));
+      let metadata = await readFile(process.env.METADATA_JSON_PATH, 'utf8');
+      metadata = metadata.split(/\r?\n/)
+        .filter(function (json) {
+          try {
+            JSON.parse(json);
+            return true;
+          } 
+          
+          catch(e) { 
+            console.error('!!! Could not parse metadata row: ', json);
+            return false;
+          }
+        })
+        .map((json) => JSON.parse(json));
+      res(metadata);
     }
 
     catch(err) {
-      console.error('!!! Error during fixPropertyName: ', err);
+      console.error('!!! Error in readMetadataFile: ', err);
       rej(err);
     }
   });
@@ -111,82 +99,129 @@ async function getFlattenedEntries(package, db) {
     let submitterDisplayName = 'none';
 
     db.collection('users').findOne({'_id': package.submitter.oid}, async function (err, submitter) {
-      if(null !== err) {
-        console.error('!!! Error in getFlattenedEntries, findOne for submitter: ', err);
+      try {
+        if(err) {
+          rej(err);
+          return;
+        }
+        
+        if(submitter) {
+          submitterEmail = submitter.email;
+          submitterDisplayName = submitter.displayName;
+        }
+
+        let metaCt = package.metadata.length;
+
+        for(let i = 0; (i == 0 && metaCt == 0) || i < metaCt; i++) {
+          let doc = {};
+          let indexId = package._id;
+
+          for(prop in package) {
+            let propName = fixPropertyName(prop);
+            propName = propName
+              .replace('metadataFile_', '')
+              .replace('_id', 'packageId');
+            doc['package_' + propName] = package[prop];
+          }
+
+          if(metaCt > 0) {
+            indexId = package.metadata[i].replicateID;
+            for(const prop in package.metadata[i]) {
+              let propName = fixPropertyName(prop);
+              doc[propName] = package.metadata[i][prop];
+            }
+          }
+
+          doc.package_submitterEmail = submitterEmail;
+          doc.package_submitterDisplayName = submitterDisplayName;
+
+          delete doc.package_metadata;
+          delete doc.package_files;
+          delete doc.package_class;
+          delete doc.package_submitter;
+
+          output.push({
+            index: { _index: process.env.ES_INDEX, _type: 'file-metadata', _id: indexId }
+          });
+
+          output.push(doc);
+        }
+
+        res(output);
+      }
+
+      catch(err) {
+        console.error('!!! Error in getFlattenedEntries: ', err);
         rej(err);
       }
-
-      else if(submitter) {
-        submitterEmail = submitter.email;
-        submitterDisplayName = submitter.displayName;
-      }
-
-      let metaCt = package.metadata.length;
-
-      for(let i = 0; (i == 0 && metaCt == 0) || i < metaCt; i++) {
-        let doc = {};
-
-        for(prop in package) {
-          let propName = await fixPropertyName(prop);
-          doc['Package_' + propName] = package[prop];
-        }
-
-        if(metaCt > 0) {
-          for(const prop in package.metadata[i]) {
-            let propName = await fixPropertyName(prop);
-            doc[propName] = package.metadata[i][prop];
-          }
-        }
-
-        doc.Submitter_email = submitterEmail;
-        doc.Submitter_displayName = submitterDisplayName;
-
-        delete doc.Package_files;
-        delete doc.Package_class;
-        delete doc.Package_submitter;
-
-        output.push({
-          index: { _index: process.env.ES_INDEX, _type: 'package-metadata', _id: fileCt > 0 ? doc.fileId : doc.packageId }
-        });
-
-        output.push(doc);
-      }
-
-      res(output);
     });
   });
+}
+
+function fixPropertyName(prop) {
+  try {
+    let fixedProp = prop.slice(0);
+    for(let replaceKey in propertyFixes.replace) {
+      fixedProp = fixedProp.replace(new RegExp(replaceKey, 'g'), propertyFixes.replace[replaceKey]);
+    }
+
+    let fixedPropWords = fixedProp.split(/(?=[A-Z])/);
+    for(let i = 0; i < fixedPropWords.length; i++) {
+      if(i == 0 || "_" === fixedPropWords[i - 1].slice(-1)) {
+        fixedPropWords[i] = fixedPropWords[i].slice(0,1).toLowerCase() + fixedPropWords[i].slice(1);
+      }
+    }
+
+    if(propertyFixes.packagePropertiesOnMetadata.indexOf(prop) > -1) {
+      fixedPropWords.unshift('metadataFile_');
+    }
+
+    return fixedPropWords.join('');
+  }
+
+  catch(err) {
+    console.error('!!! Error during fixPropertyName: ', err);
+  }
 }
 
 async function postMetadata(body) {
   return new Promise((res, rej) => {
-    esClient.ping({
-      requestTimeout: 10000
-    }, (err) => {
-      if(err) {
-        console.error('Error, cannot connect: ' + err);
+    esClient.bulk({ body }, (err, result) => {
+      if (err)  {
         rej(err);
+        return;
       }
 
-      else {
-        esClient.bulk({ body: body }, function (err, result) {
-          res();
-        });
-      }
+      res();
     });
   });
 }
 
-MongoClient.connect(process.env.MONGO_URL, {
-    'useUnifiedTopology': true,
-    'forceServerObjectId' : true
-  }, function(err, client) {
-  assert.equal(null, err);
-  console.log("successfully connected to server");
-  const db = client.db(process.env.MONGO_DBNAME);
-  getApiCallBody(db)
-    .then(postMetadata)
+function main() {
+  init(requiredEnvNames)
     .then(() => {
-      client.close();
-      process.exit();
+      MongoClient.connect(process.env.MONGO_URL, {
+          'useUnifiedTopology': true,
+          'forceServerObjectId' : true
+        }, function(err, client) {
+
+          if(err) {
+            throw(err);
+            return;
+          }
+
+          const db = client.db(process.env.MONGO_DBNAME);
+          getApiCallBody(db)
+            .then(postMetadata)
+            .then(() => {
+              client.close();
+              process.exit();
+          });
+      });
+    })
+    .catch((err) => {
+      console.log('!!! main failed: ', err);
     });
-});
+}
+
+main();
--- jsonScripts/pentaho-to-elasticsearch/run.sh
+++ jsonScripts/pentaho-to-elasticsearch/run.sh
@@ -8,5 +8,5 @@ WORKER_IMAGE=$(cd docker && \
   docker build --rm -t kingstonduo/pentaho-to-elasticsearch-worker . >/dev/null && \
   docker images -q kingstonduo/pentaho-to-elasticsearch-worker)
 
-# Run the compose with passed arguments
+# Run the container with passed arguments
 docker run --env-file=.env --env ES_INDEX=$1 --env METADATA_JSON_PATH=$2 --network=dataLake $WORKER_IMAGE
